577d410f0e34eb26468d23b5b4f508866d2d6f40
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-05-14 17:17:31 +1000

    first part of main section is ready

---
+++ js/Main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $nav = $(".nav");
+    $toggleCollapse = $(".toggle-collapse ");
+
+    /*   click event on toggle menu    */
+    $toggleCollapse.click(function(){
+        $nav.toggleClass("collapse");
+    });
+});
+
+
+
+ 
