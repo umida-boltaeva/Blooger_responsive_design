9071d71e8c0b010625d60422f60d04d39c7bdfb4
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-05-20 16:36:17 +1000

    finish the sidebar and add footer

--- js/Main.js
+++ js/Main.js
@@ -14,6 +14,17 @@ $(document).ready(function(){
         nav: true,
         navText: [$(".owl-navigation .owl-nav-prev"), $(".owl-navigation .owl-nav-next")]
     });
+
+    //scroll top
+    $(".move-up span").click(function(){
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+
+
+
 });
 
 
